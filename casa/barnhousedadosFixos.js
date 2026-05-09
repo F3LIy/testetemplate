9b30407377cff2b6b38a31b1ca25f248cea7b282
F3LIy <121820939+F3LIy@users.noreply.github.com>
2026-05-09 17:22:10 -0300

    DadosFixos

---
+++ casa/barnhousedadosFixos.js
@@ -0,0 +1,13 @@
+const dadosFixos = {
+  "anfitriao": "Arethusa",
+  "nomeCasa": "BarnHouse",
+  "capmax": "4",
+  "cantquartos": "2",
+  "nomewifi": "MEGAFIBRA_BARNHOUSE",
+  "senhawifi": "caete1444",
+  "acesso": "Entrega Pessoal",
+  "codigoacesso": "não tem",
+  "checkin": "14",
+  "checkout": "12",
+  "slug": "barnhouse"
+};
